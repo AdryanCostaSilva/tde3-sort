e67d69f6ccd167cbf7be28f0cdaf9559b2aee953
Adryan Costa Silva <adryanbrabuu@gmail.com>
2025-10-23 16:52:45 -0300

    add SelectionSort.java

--- Main.java
+++ Main.java
@@ -1,22 +1,59 @@
 public class Main {
     public static void main(String[] args){
-        int[] vetor1= {12, 18, 9, 25, 17, 31, 22, 27, 16, 13, 19, 23, 20, 30, 14, 11, 15, 24, 26, 28};
+        int[] vetor1 = {12, 18, 9, 25, 17, 31, 22, 27, 16, 13, 19, 23, 20, 30, 14, 11, 15, 24, 26, 28};
         int[] vetor2 = {5, 7, 9, 10, 12, 14, 15, 17, 19, 21, 22, 23, 24, 25, 27, 28, 29, 30, 31, 32};
         int[] vetor3 = {99, 85, 73, 60, 50, 40, 35, 30, 25, 20, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6};
 
         BubbleSort bs = new BubbleSort();
 
-        System.out.println("Vetor 1: ");
-        bs.bubbleFlag(20, vetor1);
-        printArray(vetor1);
+        int[] copiaBubble1 = copia(vetor1);
+        int[] copiaBubble2 = copia(vetor2);
+        int[] copiaBubble3 = copia(vetor3);
 
-        System.out.println("Vetor 2: ");
-        bs.bubbleFlag(20, vetor2);
-        printArray(vetor2);
+        System.out.println("===========Buble com Flag===========\n");
 
-        System.out.println("Vetor 3: ");
-        bs.bubbleFlag(20, vetor3);
-        printArray(vetor3);
+        System.out.println("---Vetor 1---");
+        bs.bubbleFlag(20, copiaBubble1);
+        printArray(copiaBubble1);
+
+        System.out.println(" ");
+
+        System.out.println("---Vetor 2---");
+        bs.bubbleFlag(20, copiaBubble2);
+        printArray(copiaBubble2);
+
+        System.out.println(" ");
+
+        System.out.println("---Vetor 3---");
+        bs.bubbleFlag(20, copiaBubble3);
+        printArray(copiaBubble3);
+
+
+        SelectionSort ss = new SelectionSort();
+
+        int[] copiaSelectionSort1 = copia(vetor1);
+        int[] copiaSelectionSort2 = copia(vetor2);
+        int[] copiaSelectionSort3 = copia(vetor3);
+
+        System.out.println(" ");
+
+        System.out.println("===========Selection Sort===========\n");
+
+        System.out.println("---Vetor 1---");
+        ss.sort(20, copiaSelectionSort1);
+        printArray(copiaSelectionSort1);
+
+        System.out.println(" ");
+
+        System.out.println("---Vetor 2---");
+        ss.sort(20, copiaSelectionSort2);
+        printArray(copiaSelectionSort2);
+
+        System.out.println(" ");
+
+        System.out.println("---Vetor 3---");
+        ss.sort(20, copiaSelectionSort3);
+        printArray(copiaSelectionSort3);
     }
 
     static void printArray(int[] vetor) {
@@ -24,5 +61,13 @@ public class Main {
             System.out.print(v + " ");
         }
         System.out.println();
+    }
+
+    private static int[] copia(int[] vetor) {
+        int[] copia = new int[vetor.length];
+        for (int i = 0; i < vetor.length; i++) {
+            copia[i] = vetor[i];
+        }
+        return copia;
     }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class SelectionSort {
+    private int iteracao;
+    private int trocas;
+    private long tempo;
+
+    public SelectionSort(){}
+
+    public void sort(int tamanho, int[] vetor){
+        int temp;
+        int trocas = 0;
+        int iteracao = 0;
+
+        long inicio = System.nanoTime();
+        for (int i = 0; i < tamanho - 1; i++){
+            int indiceMinimo = i;
+            for (int j = i + 1; j < tamanho; j++){
+                if (vetor[j] < vetor[indiceMinimo]){
+                    indiceMinimo = j;
+                }
+                iteracao++;
+            }
+
+            if (indiceMinimo != i){
+                temp = vetor[i];
+                vetor[i] = vetor[indiceMinimo];
+                vetor[indiceMinimo] = temp;
+                trocas++;
+            }
+        }
+        long fim = System.nanoTime();
+        long tempo = fim - inicio;
+
+        this.iteracao = iteracao;
+        this.trocas = trocas;
+        this.tempo = tempo;
+
+        System.out.println("Iterações: " + iteracao);
+        System.out.println("Trocas: " + trocas);
+        System.out.println("Tempo de execução: " + tempo + " ns");
+    }
+
 }
